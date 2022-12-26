8b1ef309ebcb5276e83a28de70e21ea091646f7b
Vito Stamatti <48925565+vitostamatti@users.noreply.github.com>
2022-12-26 20:14:31 +0000

    format

--- deeprec/dataset.py
+++ deeprec/dataset.py
@@ -4,76 +4,71 @@ import numpy as np
 
 
 class RecDataset:
-    
+
     def __init__(
-            self, 
-            user_id:str = 'user_id', 
-            item_id:str = 'item_id', 
-            target_id:str = 'rating'
-        ):
-            
+        self,
+        user_id: str = "user_id",
+        item_id: str = "item_id",
+        target_id: str = "rating",
+    ):
+
         self.user_id = user_id
         self.item_id = item_id
         self.target_id = target_id
-        
-        
+
     def get_params(self):
         return {
-            "user_id":self.user_id,
-            "item_id":self.item_id,
-            "target_id":self.target_id
+            "user_id": self.user_id,
+            "item_id": self.item_id,
+            "target_id": self.target_id,
         }
 
-
-    def set_params(self, user_id:str=None, item_id:str=None, target_id:str=None):
+    def set_params(
+        self,
+        user_id: str = None,
+        item_id: str = None,
+        target_id: str = None
+    ):
         self.user_id = user_id if user_id else self.user_id
         self.item_id = item_id if item_id else self.item_id
         self.target_id = target_id if target_id else self.target_id
- 
 
-    def build_x(self, X:pd.DataFrame):
-        input_dict = {} 
+    def build_x(self, X: pd.DataFrame):
+        x = {}
         for i in X.columns:
-            input_dict[i]=X[i].values
-        return input_dict
-    
-    
-    def build_y(self, y:pd.Series):
+            x[i] = X[i].values
+        return x
+
+    def build_y(self, y: pd.Series):
         return y.values
 
-
-    def build(self, X:pd.DataFrame=None, y:pd.Series=None):
+    def build(self, X: pd.DataFrame = None, y: pd.Series = None):
         x = self.build_x(X)
         y = self.build_y(y)
         return x, y
-    
-    
-    def __call__(
-            self, 
-            interactions:pd.DataFrame, 
-            user_features:pd.DataFrame=None, 
-            item_features:pd.DataFrame=None
-        ):
 
-        X = interactions[[self.user_id,self.item_id]].copy()
-        
+    def __call__(
+        self,
+        interactions: pd.DataFrame,
+        user_features: pd.DataFrame = None,
+        item_features: pd.DataFrame = None,
+    ):
+
+        X = interactions[[self.user_id, self.item_id]].copy()
+
         y = interactions[self.target_id].copy()
-        
-        if isinstance(user_features,pd.DataFrame):
+
+        if isinstance(user_features, pd.DataFrame):
             X = X.merge(
-                user_features,
-                left_on=self.user_id,
-                right_on=self.user_id,
-                how='left'
+                user_features, left_on=self.user_id, right_on=self.user_id, how="left"
             )
-        
+
         if isinstance(item_features, pd.DataFrame):
             X = X.merge(
-                item_features,
-                left_on=self.item_id,
-                right_on=self.item_id,
-                how='left'
+                item_features, left_on=self.item_id, right_on=self.item_id, how="left"
             )
-        
-        return self.build(X,y)
-    
+
+        return self.build(X, y)
+
+
+
